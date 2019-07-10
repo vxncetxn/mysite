9eee3338687cef135e6af9344d1ee09eba70e4e6
Vance <thevancetan@gmail.com>
2019-07-10 16:06:38 +0800

    remove .cache and public properly

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/vancetan/Documents/Experiments/Portfolio-Two/node_modules/gatsby-mdx/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- .cache/caches/gatsby-mdx/mdx-scopes-dir/e5c48b24ec87b4c134aaf089287490e6.js
+++
@@ -1,8 +0,0 @@
-import DefaultPara from "/Users/vancetan/Documents/Experiments/Portfolio-Two/src/components/primitives/DefaultPara.js";
-import Anchor from "/Users/vancetan/Documents/Experiments/Portfolio-Two/src/components/primitives/Anchor.js";
-import React from 'react';
-export default {
-  DefaultPara,
-  Anchor,
-  React
-};
